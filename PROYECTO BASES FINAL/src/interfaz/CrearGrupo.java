6fd77a6b8e96869207d1aca0c480a08dfa00e111
Johan Murillo <johan_smc@hotmail.com>
2016-11-20 20:35:55 -0500

    cd

--- PROYECTO BASES FINAL/src/interfaz/CrearGrupo.java
+++ PROYECTO BASES FINAL/src/interfaz/CrearGrupo.java
@@ -35,6 +35,10 @@ public class CrearGrupo extends javax.swing.JFrame {
         this.jTable1 = this.crearTablaInfo(user);
     }
 
+     public CrearGrupo( String user , short grupo ) {
+        initComponents();
+        this.jTable1 = this.crearTablaInfo(user);
+    }
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -167,6 +171,15 @@ public class CrearGrupo extends javax.swing.JFrame {
 		if(this.columNameContacts==null)
 			this.columNameContacts=new Vector<>(Arrays.asList(this.nombresContacts));
                 List< Objects > temp =  controlador.getContactos(user) ;
+                this.dataContacts = new Vector< Object >();
+                for( Object o : temp )
+                    this.dataContacts.add(o);
+		return new JTable(this.dataContacts,this.columNameContacts);
+	}
+     private JTable crearTablaInfo( String user , short grupo ) {
+		if(this.columNameContacts==null)
+			this.columNameContacts=new Vector<>(Arrays.asList(this.nombresContacts));
+              //  List< Objects > temp =  controlador.getContactos(user,grupo) ;
                 this.dataContacts = new Vector< Object >();
                 for( Object o : temp )
                     this.dataContacts.add(o);
